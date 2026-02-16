6069707098afb0695fc26dba68cee273e48634b9
Winston White <whitew2015@fau.edu>
2026-02-16 12:55:11 -0500

    added filter update logic

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import GameCard from './GameCard';
 import { useState } from 'react';
 
-//test variable
-const games = [
-  { id: 1, title: "Elden Ring", status: "Playing" },
-  { id: 2, title: "Cyberpunk 2077", status: "Backlog" },
-  { id: 3, title: "Spider-Man 2", status: "Completed" }
-];
-  
-
 function App() {
 
   //filter state
   const [filter, setFilter] = useState("All");
 
+  //test variable
+  const games = [
+  { id: 1, title: "Elden Ring", status: "Playing" },
+  { id: 2, title: "Cyberpunk 2077", status: "Backlog" },
+  { id: 3, title: "Spider-Man 2", status: "Completed" }
+  ];
 
+  const filteredGames = filter === "All" ? games :
+    games.filter(game => game.status === filter);
 
   return (
     <div className="App">
@@ -27,7 +27,7 @@ function App() {
       </div>
       
 
-      {games.map(game => (
+      {filteredGames.map(game => (
         <GameCard key={game.id} title={game.title} status={game.status} />
       ))}
       
