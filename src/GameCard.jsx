4fa7434df4330af698e75b7cfe86013dd83deacb
Winston White <whitew2015@fau.edu>
2026-02-16 13:56:58 -0500

    added delete button

--- src/GameCard.jsx
+++ src/GameCard.jsx
@@ -1,9 +1,10 @@
-export default function GameCard({ title, status }) {
+export default function GameCard({ title, status, onDelete}) {
     return (
         <div>
             <h3>{title}</h3>
             <p className={`status ${status.toLowerCase()}`}>
                 {status}</p>
+            <button onClick={onDelete}>Delete</button>
         </div>
     );
 }
