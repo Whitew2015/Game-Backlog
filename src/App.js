b49461aea4114204f666da91ba19416cac1599f3
Winston White <whitew2015@fau.edu>
2026-02-16 12:49:05 -0500

    added filter state and several buttons for different statuses

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 import GameCard from './GameCard';
+import { useState } from 'react';
 
-
+//test variable
 const games = [
   { id: 1, title: "Elden Ring", status: "Playing" },
   { id: 2, title: "Cyberpunk 2077", status: "Backlog" },
   { id: 3, title: "Spider-Man 2", status: "Completed" }
-]
+];
   
 
-
 function App() {
+
+  //filter state
+  const [filter, setFilter] = useState("All");
+
+
+
   return (
     <div className="App">
+      <div>
+        <button onClick={() => setFilter("All")}>All</button>
+        <button onClick={() => setFilter("Playing")}>Playing</button>
+        <button onClick={() => setFilter("Completed")}>Completed</button>
+        <button onClick={() => setFilter("Backlog")}>backlog</button>
+      </div>
+      
+
       {games.map(game => (
         <GameCard key={game.id} title={game.title} status={game.status} />
       ))}
